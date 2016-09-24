16fdf35dfe407f2b025f78d6225f8ef901dace60
kyardley <klynt.yardley@gmail.com>
2016-09-24 09:09:41 -0600

    Added MenuTwo ( ). See comment above the function. - Klynt

--- DataStructureGroup/Program.cs
+++ DataStructureGroup/Program.cs
@@ -37,6 +37,7 @@ namespace DataStructureGroup
                 {
                     Console.Write("Invalid input. Try again");
                     Console.Write("->");
+                    sInput = Console.ReadLine();
                 }
                 
             }
@@ -48,24 +49,68 @@ namespace DataStructureGroup
 
         }
 
+        /* I had to leave this before I could add comments.  I am pretty sure it works but I am going to test it more. 
+         * Essentially it brings up the Second menu and uses "Queue", Dictionary", etc. from menu one.
+         * I will add more documentary on it later.
+         */
+        public static int menuTwo(string StructureType)
+        {
+            Console.Write("Please choose an option: \n");
+            Console.Write("1. Add one time to " + StructureType + "\n");
+            Console.Write("2. Add Huge List of Items to " + StructureType + "\n");
+            Console.Write("3. Display " + StructureType + "\n");
+            Console.Write("4. Delete from " + StructureType + "\n");
+            Console.Write("5. Clear " + StructureType + "\n");
+            Console.Write("6. Search " + StructureType + "\n");
+            Console.Write("7. Return to Main Menu\n");
+            Console.Write("->");
+
+            string sInput;
+            sInput = Console.ReadLine();
+            int option = -1;
+
+            bool validOption = false;
+            while (!validOption)
+            {
+                try
+                {
+                    option = int.Parse(sInput);
+                    validOption = true;
+                }
+                catch
+                {
+                    Console.Write("Invalid input. Try again");
+                    Console.Write("->");
+                    sInput = Console.ReadLine();
+                }
+
+            }
+
+            if (option > 0 && option < 8)
+                return option;
+            else
+                return -1;
+        }
+
 
         static void Main(string[] args)
         {
-            int option = menu(1);
+            int option1 = menu(1);
+            int option2 = -1;
 
-            switch (option)
+            switch (option1)
             {
                 case 1:
-                    Console.WriteLine("1");
+                    option2 = menuTwo("Stack");
                     break;
                 case 2:
-                    Console.WriteLine("2");
+                    option2 = menuTwo("Queue");
                     break;
                 case 3:
-                    Console.WriteLine("3");
+                    option2 = menuTwo("Dictionary");
                     break;
                 case 4:
-                    Console.WriteLine("4");
+                    
                     break;
             }
             Console.ReadKey();
